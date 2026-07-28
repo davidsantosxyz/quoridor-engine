02be86aea779af6841f1a862428729d897529072
David Santos <d.samuelsantos9@gmail.com>
2026-07-28 21:08:43 +0100

    mesh_coord_to_num converts the coordinates of a vertex into its number when we start counting from the top left corner from left to right and top to bottom

--- graphs.py
+++ graphs.py
@@ -15,3 +15,10 @@ def mesh_graph(side_length: int):
 
     return mesh
 
+def mesh_coord_to_num(x, y, side_length: int) -> int:
+    for var_name, var_value in (('x', x),('y', y)):
+        if not 0 <= var_value < side_length:
+            raise ValueError(f'{var_name} must be between {0} and {side_length - 1}')
+
+    return x * side_length + y
+

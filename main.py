7d1673d98b87e0806f35c1843a8b737f0244aae1
davidsantosxyz <d.samuelsantos9@gmail.com>
2026-08-01 21:23:29 +0100

    Updated main with print_board

--- main.py
+++ main.py
@@ -3,16 +3,9 @@ import config
 
 board = Board(config.BOARD_SIZE)
 
-player1 = Player(pawn_color='white')
-board.add_pawn(player1.pawn, 8, 4)
-
-player2 = Player(pawn_color='black')
-board.add_pawn(player2.pawn, 0, 4)
-
 action = ''
 while action != 'quit':
-    board.print_squares()
-    board.print_lattice()
+    board.print_board()
     action = input('Action:')
 
 
